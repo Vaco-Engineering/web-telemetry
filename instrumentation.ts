84855fbc050ab95eba9e84ab707bb282c9d8cc57
Joshua Schniper <joshua.schniper@chiprewards.com>
2023-01-04 09:18:40 -0600

    Cleanup, web telemetry moved to a lib, beginnings of CI

---
+++ instrumentation.ts
@@ -0,0 +1,54 @@
+import api from '@opentelemetry/api';
+import { WebTracerProvider } from '@opentelemetry/sdk-trace-web';
+import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import { ZoneContextManager } from '@opentelemetry/context-zone';
+import { B3Propagator } from '@opentelemetry/propagator-b3';
+import { Resource } from '@opentelemetry/resources';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+
+interface InstrumentationConfig {
+  name: string;
+  namespace?: string;
+  version?: string;
+  id?: string;
+  url?: string;
+}
+
+export default {
+  config: (config: InstrumentationConfig) => {
+    const resource = Resource.default().merge(
+      new Resource({
+        [SemanticResourceAttributes.SERVICE_INSTANCE_ID]: config.id,
+        [SemanticResourceAttributes.SERVICE_NAME]: config.name,
+        [SemanticResourceAttributes.SERVICE_NAMESPACE]: config.namespace,
+        [SemanticResourceAttributes.SERVICE_VERSION]: config.version
+      })
+    );
+
+    const provider: WebTracerProvider = new WebTracerProvider({
+      resource: resource
+    });
+
+    const exporter = new OTLPTraceExporter({
+      url: config.url || 'http://localhost:5173/v1/traces'
+    });
+
+    provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+
+    provider.register({
+      contextManager: new ZoneContextManager()
+    });
+
+    // Registering instrumentations
+    registerInstrumentations({
+      instrumentations: [getWebAutoInstrumentations()]
+    });
+
+    api.propagation.setGlobalPropagator(new B3Propagator());
+
+    return api;
+  }
+};
