2da804ef9fa82d372d1f97ef8615a7c0fa9e0764
Joshua Schniper <joshua.schniper@chiprewards.com>
2023-01-31 16:51:35 -0600

    Shared ESLint config that seems to work. Seemingly.

---
+++ .eslintrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+  extends: ['@vacobuilt/eslint-config']
+};
--- instrumentation.ts
+++ instrumentation.ts
@@ -4,7 +4,7 @@ import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
 import { registerInstrumentations } from '@opentelemetry/instrumentation';
-import { ZoneContextManager } from '@opentelemetry/context-zone';
+// import { ZoneContextManager } from '@opentelemetry/context-zone';
 import { B3Propagator } from '@opentelemetry/propagator-b3';
 import { Resource } from '@opentelemetry/resources';
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
@@ -19,36 +19,39 @@ interface InstrumentationConfig {
 
 export default {
   config: (config: InstrumentationConfig) => {
-    const resource = Resource.default().merge(
-      new Resource({
-        [SemanticResourceAttributes.SERVICE_INSTANCE_ID]: config.id,
-        [SemanticResourceAttributes.SERVICE_NAME]: config.name,
-        [SemanticResourceAttributes.SERVICE_NAMESPACE]: config.namespace,
-        [SemanticResourceAttributes.SERVICE_VERSION]: config.version
-      })
-    );
+    if (config.url) {
+      const resource = Resource.default().merge(
+        new Resource({
+          [SemanticResourceAttributes.SERVICE_INSTANCE_ID]: config.id,
+          [SemanticResourceAttributes.SERVICE_NAME]: config.name,
+          [SemanticResourceAttributes.SERVICE_NAMESPACE]: config.namespace,
+          [SemanticResourceAttributes.SERVICE_VERSION]: config.version
+        })
+      );
 
-    const provider: WebTracerProvider = new WebTracerProvider({
-      resource: resource
-    });
+      const provider: WebTracerProvider = new WebTracerProvider({
+        resource: resource
+      });
 
-    const exporter = new OTLPTraceExporter({
-      url: import.meta.env.VITE_OTEL_EXPORTER_OTLP_ENDPOINT
-    });
+      const exporter = new OTLPTraceExporter({
+        url: config.url
+      });
 
-    provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+      provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
 
-    provider.register({
-      contextManager: new ZoneContextManager()
-    });
+      provider.register({
+        // NOTE: This seems to cause problem with data fetcing and Jotai (maybe other things as well)
+        // contextManager: new ZoneContextManager()
+      });
 
-    // Registering instrumentations
-    registerInstrumentations({
-      instrumentations: [getWebAutoInstrumentations()]
-    });
+      // Registering instrumentations
+      registerInstrumentations({
+        instrumentations: [getWebAutoInstrumentations()]
+      });
 
-    api.propagation.setGlobalPropagator(new B3Propagator());
+      api.propagation.setGlobalPropagator(new B3Propagator());
 
-    return api;
+      return api;
+    }
   }
 };
